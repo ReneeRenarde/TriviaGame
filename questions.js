32baaff1e9b4cd5216e53b42e5a7ed404419c765
Ugni Blanc <ajo2185@gmail.com>
2020-03-05 00:45:09 -0700

    added list of questions

--- questions.js
+++ questions.js
@@ -1,17 +1,106 @@
-var questions [{
-    "question": "?",
-    "option1": "",
-    "option2": "",
-    "option3": "",
-    "option4": "",
+var questions = [{
+    "question": "How many compartments does a cow’s stomach have?",
+    "option1": "Three",
+    "option2": "Four",
+    "option3": "Six",
+    "option4": "One",
     "answer": "2"
 }, {
-    
-}
-
-
-
-
-
-
-]
+        "question": "Which is the largest mammal in the world?",
+        "option1": "The hippopotamus",
+        "option2": "The giraffe",
+        "option3": "The blue whale",
+        "option4": "The bison",
+        "answer": "3"
+}, {
+        "question": "Which bird has the largest wingspan of any living bird?",
+        "option1": "The wandering albatross",
+        "option2": "The andrean condor",
+        "option3": "The dalmatian pelican",
+        "option4": "The emporer penguin",
+        "answer": "2"
+}, {
+        "question": "Which is the fastest land animal?",
+        "option1": "The cheetah",
+        "option2": "The greyhound",
+        "option3": "The lion",
+        "option4": "The pronghorn",
+        "answer": "2"
+}, {
+        "question": "What type of animal is a Mexican hairless?",
+        "option1": "A cat",
+        "option2": "A bat",
+        "option3": "A hog",
+        "option4": "A dog",
+        "answer": "4"
+}, {
+        "question": "What kind of creature is a Portuguese man o’ war?",
+        "option1": "A stingray",
+        "option2": "A lizard",
+        "option3": "A jellyfish",
+        "option4": "A bird",
+        "answer": "3"
+}, {
+        "question": "How many legs does a lobster have?",
+        "option1": "10",
+        "option2": "6",
+        "option3": "4",
+        "option4": "8",
+        "answer": "1"
+}, {
+        "question": "What type of animal is a Flemish giant?",
+        "option1": "A dog",
+        "option2": "A rabbit",
+        "option3": "A hamster",
+        "option4": "A horse",
+        "answer": "2"
+}, {
+        "question": "Which creatures produce gossamer?",
+        "option1": "Mandrills",
+        "option2": "Spiders",
+        "option3": "Hummingbirds",
+        "option4": "Rosemallows",
+        "answer": "2"
+}, {
+        "question": "From which animal is mohair obtained?",
+        "option1": "The angora rabbit",
+        "option2": "The horse",
+        "option3": "The angora goat",
+        "option4": "The hairbrush",
+        "answer": "3"
+}, {
+        "question": "Whereabouts do demersal fish live in the sea",
+        "option1": "Near the top",
+        "option2": "Near the middle",
+        "option3": "Near the bottom",
+        "option4": "These fish don't live in the sea",
+        "answer": "3"
+}, {
+        "question": "How is the domestic cavy better known?",
+        "option1": "As the guinea pig",
+        "option2": "As the chinchilla",
+        "option3": "As the hermit crab",
+        "option4": "As the sugar glider",
+        "answer": "1"
+}, {
+        "question": "How many bones does the giraffe have in its neck?",
+        "option1": "14",
+        "option2": "7",
+        "option3": "12",
+        "option4": "4",
+        "answer": "2"
+}, {
+        "question": "A parliament is the name given to a group of which bird?",
+        "option1": "The finch",
+        "option2": "The parrot",
+        "option3": "The swallow",
+        "option4": "The owl",
+        "answer": "4"
+}, {
+        "question": "Which is the largest living bird?",
+        "option1": "The ostrich",
+        "option2": "The albatross",
+        "option3": "The bald eagle",
+        "option4": "The king penguin",
+        "answer": "2"
+}]
